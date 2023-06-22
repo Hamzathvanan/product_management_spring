2c89659c18c83d1878d1bfd5fb2cdb664f61b2a2
Hamzathvanan <hamzathvanan@gmail.com>
2023-06-22 10:28:58 +0530

    Initial ProductSpring project

---
+++ src/test/java/com/hamzath/ProductSpring/ProductSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamzath.ProductSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
